bc6234f6a8feb740012d6ea9bdafbddefecd7ab5
Derpimus <michal.piotr.wysocki@gmail.com>
2021-11-18 00:44:46 +0100

    Add files

---
+++ script.js
@@ -0,0 +1,69 @@
+window.onload = function()
+{
+	drawList();
+};
+
+var give = ['Makrzak','Knach','Adelka','Piosik','Seyda','Ewa','Zuzia','Michal','Emilka','Dzięgo','Apollo','Mareł','Tocha','Ineł','Artur','Sawko','Kacper','Beon','Owoc'];
+var receive = give.concat();
+var peopleWrap = document.getElementById('peopleWrap');
+var people = document.getElementById('people');
+var choose = document.getElementById('choose');
+var result = document.getElementById('result');
+var close = document.getElementById('close');
+
+function drawList()
+{
+	people.innerHTML = '<option value="">Kto losuje?</option>';
+	for (var i = give.length - 1; i >= 0; i--) {
+		var option = document.createElement('option');
+		option.value = i;
+		option.innerHTML = give[i];
+		people.appendChild(option);
+	}
+}
+
+function selectPerson(person) 
+{
+	var name = give[person];
+	var nameIndex = receive.indexOf(name);
+	
+	if(nameIndex >= 0) 
+	{
+		receive.splice(nameIndex, 1);
+	}
+	var recipient = Math.floor((Math.random() * receive.length));
+	var recipientName = receive[recipient];
+	
+	receive.splice(recipient, 1);
+	give.splice(person, 1);
+
+	if(nameIndex >= 0)
+	{
+		receive.push(name);
+	}
+	result.innerHTML += "<h2>" + name + " kupuje prezent " + recipientName + "!</h2>";
+	if(give.length > 0)
+	{
+		drawList();
+	}
+}
+
+choose.onclick = function()
+{
+	if(people.value)
+	{
+		selectPerson(people.value);
+	}
+};
+
+close.onclick = function()
+{
+	result.innerHTML = "";
+	close.innerHTML = "";
+  if(give.length == 0){
+ peopleWrap.parentNode.removeChild(peopleWrap);
+		choose.parentNode.removeChild(choose);
+		result.innerHTML = "<h2>All done!</h2>";
+		close.innerHTML = "";
+	}
+};
